0f945b680953ce44908ec1b40549ca159a40f39f
Fazalul Abid <fazalulabid007@gmail.com>
2024-03-16 01:30:19 +0530

    feat: Implemented collapsed player background gradient

--- app/src/main/java/com/fazalulabid/samespacemusic/presentation/screens/player/PlayerCollapsedContent.kt
+++ app/src/main/java/com/fazalulabid/samespacemusic/presentation/screens/player/PlayerCollapsedContent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -18,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
@@ -34,6 +36,7 @@ import com.fazalulabid.samespacemusic.presentation.ui.theme.SizeSmall8
 import com.fazalulabid.samespacemusic.presentation.ui.theme.SizeStandard16
 import com.fazalulabid.samespacemusic.presentation.ui.theme.SizeTiny2
 import com.fazalulabid.samespacemusic.presentation.ui.theme.StandardScreenPadding
+import com.fazalulabid.samespacemusic.presentation.util.fromHex
 
 @Composable
 fun PlayerCollapsedContent(
@@ -45,50 +48,72 @@ fun PlayerCollapsedContent(
     onClick: () -> Unit,
     onActionClick: () -> Unit
 ) {
-    Row(
-        modifier = Modifier
+    Box(
+        modifier = modifier
             .fillMaxWidth()
-            .clickable { onClick() }
-            .background(Color.Black)
-            .padding(horizontal = StandardScreenPadding, vertical = SizeSmall8),
-        horizontalArrangement = Arrangement.Absolute.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+            .background(MaterialTheme.colorScheme.background)
     ) {
         Row(
-            modifier
-                .weight(1f),
+            modifier = Modifier
+                .clickable { onClick() }
+                .background(
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            Color
+                                .fromHex(currentMusicTrack.accent)
+                                .copy(alpha = 0.7f),
+                            Color
+                                .fromHex(currentMusicTrack.accent)
+                                .copy(alpha = 0.8f),
+                            Color
+                                .fromHex(currentMusicTrack.accent)
+                                .copy(alpha = 0.9f)
+                        )
+                    )
+                )
+                .padding(horizontal = StandardScreenPadding, vertical = SizeSmall8),
+            horizontalArrangement = Arrangement.Absolute.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Image(
-                painter = rememberAsyncImagePainter(
-                    model = currentMusicTrack.getCoverImageUrl(),
-                    imageLoader = imageLoader
-                ),
-                contentDescription = stringResource(R.string.cover_image, currentMusicTrack.name),
-                modifier = Modifier
-                    .size(thumbnailSize)
-                    .clip(thumbnailShape),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.width(StandardScreenPadding))
-            Text(
-                text = currentMusicTrack.name,
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    color = MaterialTheme.colorScheme.onBackground
-                ),
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+            Row(
+                modifier
+                    .weight(1f),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = currentMusicTrack.getCoverImageUrl(),
+                        imageLoader = imageLoader
+                    ),
+                    contentDescription = stringResource(
+                        R.string.cover_image,
+                        currentMusicTrack.name
+                    ),
+                    modifier = Modifier
+                        .size(thumbnailSize)
+                        .clip(thumbnailShape),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(StandardScreenPadding))
+                Text(
+                    text = currentMusicTrack.name,
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        color = MaterialTheme.colorScheme.onBackground
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            StandardIconButton(
+                onClick = {
+                    onActionClick()
+                },
+                icon = if (true) {
+                    R.drawable.pause
+                } else R.drawable.play,
+                color = MaterialTheme.colorScheme.background,
+                backgroundColor = MaterialTheme.colorScheme.onBackground
             )
         }
-        StandardIconButton(
-            onClick = {
-                onActionClick()
-            },
-            icon = if (true) {
-                R.drawable.pause
-            } else R.drawable.play,
-            color = MaterialTheme.colorScheme.background,
-            backgroundColor = MaterialTheme.colorScheme.onBackground
-        )
     }
 }
