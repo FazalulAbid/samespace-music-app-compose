1287262d5bc1022e9d07a00f46495cdde18e0e1d
Fazalul Abid <fazalulabid007@gmail.com>
2024-03-16 16:32:50 +0530

    fix: Fixed status bar color issue on theme change

--- app/src/main/java/com/fazalulabid/samespacemusic/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/fazalulabid/samespacemusic/presentation/ui/theme/Theme.kt
@@ -1,8 +1,15 @@
 package com.fazalulabid.samespacemusic.presentation.ui.theme
 
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -17,10 +24,18 @@ private val DarkColorScheme = darkColorScheme(
 fun SamespaceMusicTheme(
     darkTheme: Boolean = true,
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = DarkColorScheme
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 
     MaterialTheme(
         colorScheme = colorScheme,
